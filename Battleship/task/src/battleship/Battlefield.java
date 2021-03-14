cc60337fe50dcde993b96fb7d8f6b47f1535146e
johnhasgone <eu.vaganov@gmail.com>
2021-03-14 22:42:22 +0300

    Stage 5 : two players mode implemented

--- Battleship/task/src/battleship/Battlefield.java
+++ Battleship/task/src/battleship/Battlefield.java
@@ -9,16 +9,36 @@ public class Battlefield {
     private char hit = 'X';  // hit cell of the ship
     private char miss = 'M'; // missed hit
     private int height = 10; // height of the battlefield
-    private int width = 10;   // wide of the battlefield
+    private int width = 10;  // wide of the battlefield
+    private Player player1;  // first player
+    private Player player2;  // second player
 
-    private char[][] field = new char[height][width]; // main game field, ships are hidden from player
-    private final char[][] fogField = new char[height][width]; // game field, showing players shots without ships
-    
-    private final UserInputHandler inputHandler = new UserInputHandler();
+    private final UserInputHandler inputHandler = new UserInputHandler(); // object for handling user input
 
     public Battlefield() {
-        initField(field);
-        initField(fogField);
+        player1 = new Player("Player 1");
+        player2 = new Player("Player 2");
+        initField(player1.getField());
+        initField(player1.getFogField());
+        initField(player2.getField());
+        initField(player2.getFogField());
+
+
+    }
+
+    public Battlefield(char fog, char cell, char hit, char miss, int height, int width, String player1, String player2) {
+        this.fog = fog;
+        this.cell = cell;
+        this.hit = hit;
+        this.miss = miss;
+        this.height = height;
+        this.width = width;
+        this.player1 = new Player(player1);
+        this.player2 = new Player(player2);
+        initField(this.player1.getField());
+        initField(this.player1.getFogField());
+        initField(this.player2.getField());
+        initField(this.player2.getFogField());
     }
 
     /**
@@ -102,15 +122,6 @@ public class Battlefield {
     }
 
     /**
-     * Set the game field height
-     * @param height game field height
-     */
-    public void setHeight(int height) {
-        this.height = height;
-        this.field = new char[height][width];
-    }
-
-    /**
      * get the game field width
      * @return game field width
      */
@@ -119,20 +130,35 @@ public class Battlefield {
     }
 
     /**
-     * Set the game field width
-     * @param width game field width
+     * get the first player
+     * @return first player
      */
-    public void setWidth(int width) {
-        this.width = width;
-        this.field = new char[height][width];
+    public Player getPlayer1() {
+        return player1;
     }
 
     /**
-     * Get the game field
-     * @return field
+     * set the first player
+     * @param player1 first player
      */
-    public char[][] getField() {
-        return field;
+    public void setPlayer1(Player player1) {
+        this.player1 = player1;
+    }
+
+    /**
+     * get the second player
+     * @return second player
+     */
+    public Player getPlayer2() {
+        return player2;
+    }
+
+    /**
+     * set the second player
+     * @param player2 second player
+     */
+    public void setPlayer2(Player player2) {
+        this.player2 = player2;
     }
 
     /**
@@ -162,8 +188,9 @@ public class Battlefield {
      * @param rowTo number of field row where ship placement ends (from 0 to height exclusively)
      * @param columnFrom number of field column where ship placement starts (from 0 to width exclusively)
      * @param columnTo number of field column where ship placement ends (from 0 to width exclusively)
+     * @param field battlefield where the ship should be placed
      */
-    public void setTheShip(int rowFrom, int rowTo, int columnFrom, int columnTo) {
+    public void setTheShip(int rowFrom, int rowTo, int columnFrom, int columnTo, char[][] field) {
         for (int i = rowFrom; i < rowTo + 1; i++) {
             Arrays.fill(field[i], columnFrom, columnTo + 1, cell);
         }
@@ -172,13 +199,35 @@ public class Battlefield {
 
     /**
      * Filling the battlefield with all the ships
+     * @param player the player, on which field the ships should be placed
      */
-    public void fillTheField() {
+    public void fillTheField(Player player) {
+        System.out.printf("%s, place your ships on the game field\n", player.getName());
+        printField(player.getField());
         for (Ships ship : Ships.values()) {
-            int[] coordinates = inputHandler.getShipCoordinates(ship);
-            setTheShip(coordinates[0], coordinates[2], coordinates[1], coordinates[3]);
-            printField(field);
+            int[] coordinates = inputHandler.getShipCoordinates(ship, player);
+            setTheShip(coordinates[0], coordinates[2], coordinates[1], coordinates[3], player.getField());
+            printField(player.getField());
         }
+    }
+
+    /**
+     * place all the ships of both players
+     */
+    public void fillPlayersFields() {
+        fillTheField(player1);
+        passTheMove();
+        fillTheField(player2);
+        passTheMove();
+    }
+
+    /**
+     * stop the game before player presses Enter for passing the move to another one
+     */
+    public void passTheMove() {
+        System.out.println("Press Enter and pass the move to another player\n");
+        inputHandler.scanner.nextLine();
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     }
 
     /**
@@ -186,29 +235,52 @@ public class Battlefield {
      */
     public void startTheGame() {
         System.out.println("\nThe game starts!");
-        printField(fogField);
 
-        System.out.println("\nTake a shot!");
-        int aliveCells = Arrays.stream(Ships.values())
+        // setting current player, current foe and number of alive cells of current foe
+        Player currentPlayer = player1;
+        Player currentFoe = player2;
+        int aliveCellsPlayer1 = Arrays.stream(Ships.values())
                 .map(Ships::getValue)
                 .reduce(0, Integer::sum);
+        int aliveCellsPlayer2 = aliveCellsPlayer1;
+        int currentAliveCells = aliveCellsPlayer2;
 
-        while(aliveCells > 0) {
-            ShotResult shotResult = takeAShot();
-            printField(fogField);
+        // taking a shot
+        while(true) {
+            printField(currentFoe.getFogField());
+            System.out.println("---------------------");
+            printField(currentPlayer.getField());
+
+            System.out.printf("\n%s, it's your turn:\n", currentPlayer.getName());
+            ShotResult shotResult = takeAShot(currentFoe);
             if (shotResult == ShotResult.HIT) {
-                aliveCells--;
-                System.out.println("\nYou hit a ship! Try again:");
+                currentAliveCells--;
+                System.out.println("\nYou hit a ship!");
             } else if (shotResult == ShotResult.SANK) {
-                aliveCells--;
-                System.out.println(
-                        aliveCells != 0
-                        ? "\nYou sank a ship! Specify a new target:"
-                        : "\nYou sank the last ship. You won. Congratulations!"
-                );
+                currentAliveCells--;
+                if (currentAliveCells != 0)
+                    System.out.println("\nYou sank a ship!");
+                else {
+                    System.out.println("\nYou sank the last ship. You won. Congratulations!");
+                    return;
+                }
             } else {
-                System.out.println("\nYou missed! Try again:");
+                System.out.println("\nYou missed!");
             }
+
+            // change current player
+            if (currentPlayer == player1) {
+                currentPlayer = player2;
+                currentFoe = player1;
+                aliveCellsPlayer2 = currentAliveCells;
+                currentAliveCells = aliveCellsPlayer1;
+            } else {
+                currentPlayer = player1;
+                currentFoe = player2;
+                aliveCellsPlayer1 = currentAliveCells;
+                currentAliveCells = aliveCellsPlayer2;
+            }
+            passTheMove();
         }
     }
 
@@ -216,13 +288,16 @@ public class Battlefield {
      * Take a shot
      * @return ShotResult result of the shot (MISS, SANK or HIT)
      */
-    private ShotResult takeAShot() {
+    private ShotResult takeAShot(Player currentFoe) {
         int[] coord = inputHandler.getShotCoordinates();
+        char[][] field = currentFoe.getField();
+        char[][] fogField = currentFoe.getFogField();
         ShotResult result;
+
         if (field[coord[0]][coord[1]] == cell) {
             field[coord[0]][coord[1]] = hit;
             fogField[coord[0]][coord[1]] = hit;
-            if (checkShipSank(coord))
+            if (checkShipSank(coord, field))
                 result = ShotResult.SANK;
             else
                 result = ShotResult.HIT;
@@ -242,7 +317,7 @@ public class Battlefield {
      * @param coord successive shot coordinates
      * @return true if there is no ship cells next to successive shot coordinates, false otherwise
      */
-    private boolean checkShipSank(int[] coord) {
+    private boolean checkShipSank(int[] coord, char[][] field) {
 
         return (coord[0] == 0 || field[coord[0] - 1][coord[1]] != cell) &&
                 (coord[0] == (height - 1) || field[coord[0] + 1][coord[1]] != cell) &&
@@ -258,19 +333,18 @@ public class Battlefield {
      */
     class UserInputHandler {
         private final Scanner scanner = new Scanner(System.in);
-        private final int[] coordinates = new int[4];
 
         /**
          * Get the ship coordinates from user input
          * @param ship the ship which coordinates we want to get
          * @return array with ship coordinates
          */
-        public int[] getShipCoordinates(Ships ship) {
+        public int[] getShipCoordinates(Ships ship, Player player) {
             System.out.printf("\nEnter the coordinates of the %s (%d cells):\n", ship.getDescription(), ship.getValue());
             String stringCoordinates;
             do {
                 stringCoordinates = scanner.nextLine();
-            } while (!checkCoordinates(stringCoordinates, ship));
+            } while (!checkCoordinates(stringCoordinates, ship, player));
 
             return getIntCoordinatesFromString(stringCoordinates);
         }
@@ -281,7 +355,7 @@ public class Battlefield {
          * @param ship ship type
          * @return result of the checking
          */
-        private boolean checkCoordinates(String coordinates, Ships ship) {
+        private boolean checkCoordinates(String coordinates, Ships ship, Player player) {
 
             // check the initial format of user input
             if (!coordinates.matches("[A-J]\\d+ [A-J]\\d+"))
@@ -297,7 +371,7 @@ public class Battlefield {
                     System.out.printf("Error! Wrong length of the %s! Try again:\n", ship.getDescription());
 
                 // check if the coordinates are close to or crossing another ship
-                else if (!checkShipPlacement(intCoordinates))
+                else if (!checkShipPlacement(intCoordinates, player.getField()))
                     System.out.println("Error! You placed it too close to another one. Try again:\n");
 
                 else
@@ -311,7 +385,7 @@ public class Battlefield {
          * @param coord ship coordinates
          * @return result of the checking
          */
-        private boolean checkShipPlacement(int[] coord) {
+        private boolean checkShipPlacement(int[] coord, char[][] field) {
             int iStart = coord[0] != 0 ? coord[0] - 1 : coord[0];
             int iEnd = coord[2] != height - 1 ? coord[2] + 1 : coord[2];
             int jStart = coord[1] != 0 ? coord[1] - 1 : coord[1];
@@ -389,6 +463,34 @@ public class Battlefield {
         HIT,
         SANK,
         MISS
+    }
+
+    class Player {
+        private String name;
+        private final char[][] field = new char[height][width]; // main game field, ships are hidden from player
+        private final char[][] fogField = new char[height][width]; // game field, showing players shots without ships
+
+        public Player(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public char[][] getField() {
+            return field;
+        }
+
+        public char[][] getFogField() {
+            return fogField;
+        }
+
+
     }
 
 }
